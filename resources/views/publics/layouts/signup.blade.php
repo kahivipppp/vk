c089866b8b9b97261135bf8cb29d2fa4473c3380
nqk1012 <nqkhai1012@gmail.com>
2023-10-02 21:42:08 +0700

    khai

--- resources/views/publics/layouts/signup.blade.php
+++ resources/views/publics/layouts/signup.blade.php
@@ -1,17 +1,17 @@
     @extends('publics.public')
     @section('content')
-        {{ $content }}
+    {{ $content }}
 
-                <form action="/signup_post" method="post">
-                    @csrf
-                    <h2>Đăng ký</h2>
-                    <label>Họ và tên</label><br>
-                    <input type="text" name="name"><br>
-                    <label>Số điện thoại</label><br>
-                    <input type="text" name="phone"><br>
-                    <label>Email</label><br>
-                    <input type="text" name="email"><br>
-                    <button id="submit" type="submit"> Gửi </button>
-                </form>
+    <form action="/signup_post" method="post">
+        @csrf
+        <h2>Đăng ký</h2>
+        <label>Họ và tên</label><br>
+        <input type="text" name="name"><br>
+        <label>Số điện thoại</label><br>
+        <input type="text" name="phone"><br>
+        <label>Email</label><br>
+        <input type="text" name="email"><br>
+        <button id="submit" type="submit"> Gửi </button>
+    </form>
 
     @endsection
