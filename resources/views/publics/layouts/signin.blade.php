2aefd2fa702bcb64ec705987fe2445c8ad7a2cd8
nqk1012 <nqkhai1012@gmail.com>
2023-10-07 23:20:48 +0700

    kahi pull code

--- resources/views/publics/layouts/signin.blade.php
+++ resources/views/publics/layouts/signin.blade.php
@@ -4,17 +4,17 @@
 
 
 <form action="/signin_post" method="post">
-	@csrf
-	<h2>Đăng Nhập</h2>
-	<label">Họ và tên</label><br>
-		<input type="text" name="name"><br>
-		<label>Số điện thoại</label><br>
-		<input type="text" name="phone"><br>
-		<label">Email</label><br>
-			<input type="text" name="email"><br><br>
-			<a href="">Quên mật khẩu</a> <br> <br>
-			<a href="">Đăng Ký</a> <br><br>
-			<button id="submit" type="submit"> Gửi </button>
+    @csrf
+    <h2>Đăng Nhập</h2>
+    <label">Họ và tên</label><br>
+        <input type="text" name="name"><br>
+        <label>Số điện thoại</label><br>
+        <input type="text" name="phone"><br>
+        <label">Email</label><br>
+            <input type="text" name="email"><br><br>
+            <a href="">Forgot password </a> <br> <br>
+            <a href="">Đăng Ký</a> <br><br>
+            <button id="submit" type="submit"> Gửi </button>
 </form>
 
 
